538bdbd680a7badb0c5ee8a692d0f12059cde48b
fabioqr <fabioqr@outllok.com.br>
2022-07-14 15:02:15 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from "next/link";
 function Home(){
-    return <div>Hello Mundo!</div>
+    return (
+    <div>
+        <h1> Hello Mundo! </h1>
+    <Link href="/sobre">
+    <a>Acessar Sobre</a>
+    </Link>
+    
+    </div>
+    )
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+function Sobre() {
+    return (
+        <div>
+            <h1> Hello Mundo! </h1>
+            <Link href="/">
+                <a>Acessar Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
